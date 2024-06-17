420e014d0ad9e99fd491e9af1ce42872cca01daf
Anne <44207112+anne-df@users.noreply.github.com>
2024-06-17 13:57:58 +0200

    Update initialization.py
    Reconstruction loss correction

--- src/initialization.py
+++ src/initialization.py
@@ -6,7 +6,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Mean-Squared Error as the average difference between the pixels
 # in the original image vs. the reconstructed one
-criterion = nn.MSELoss()
+criterion = nn.MSELoss(reduction='sum')
 # pixel-wise MSE loss
 pixel_wise_criterion = nn.MSELoss(reduction='none')
 
